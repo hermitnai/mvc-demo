e77c7daadda3fd6ac16e9ef77b42b72605edf142
hermitnai <972105904@qq.com>
2021-12-25 21:41:39 +0800

    init

---
+++ src/app2.js
@@ -0,0 +1,15 @@
+import $ from "jquery";
+import './app2.css'
+
+const $tabBar = $("#app2 .tab-bar");
+const $tabContent = $("#app2 .tab-content")
+
+$tabBar.on("click","li",(e)=>{
+    let $li = $(e.currentTarget)
+    $li.addClass('selected').siblings().removeClass('selected')
+    const index = $li.index()
+    console.log(index)
+    $tabContent.children().eq(index).addClass('active').siblings().removeClass('active')
+})
+
+$tabBar.children().eq(0).trigger('click')
